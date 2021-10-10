0b999cc59a50d5cfae1c8d661b9ac2842ddef36e
kitasio <epickitus@pm.me>
2021-10-10 23:45:37 +0300

    second

--- src/components/nav/NavLinks.svelte
+++ src/components/nav/NavLinks.svelte
@@ -9,6 +9,6 @@
 
 <div class="hidden lg:flex items-center justify-end mx-20 space-x-10 text-green-dark text-lg">
     {#each links as i}
-        <a href={i.link} alt={i.name} class="uppercase hover:font-bold transition-all duration-300">{i.name}</a>
+        <a href={i.link} alt={i.name} class="uppercase hover:font-extrabold transition-all duration-300 hover:text-green">{i.name}</a>
     {/each}
 </div>
