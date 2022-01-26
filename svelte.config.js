ab22bc2329ed8d065384ab056e887ef5abe02d43
Kitasio <kit.plotits@gmail.com>
2022-01-26 15:02:44 +0300

    updated sveltekit to adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,49 @@
-import preprocess from 'svelte-preprocess';
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+// import preprocess from 'svelte-preprocess';
+// // svelte.config.js
+// import adapter from '@sveltejs/adapter-static';
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options are shown
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: null
+// 		}),
 
+// 		vite: {
+// 			css: {
+// 				preprocessorOptions: {
+// 					scss: {
+// 						additionalData: '@import "src/variables.scss";'
+// 					}
+// 				}
+// 			}
+// 		}
+// 	},
+
+// 	preprocess: [
+// 		preprocess({
+// 			scss: {
+// 				prependData: '@import "src/variables.scss";'
+// 			}
+// 		})
+// 	]
+// };
+
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
 		vite: {
 			css: {
 				preprocessorOptions: {
@@ -20,12 +54,7 @@ export default {
 			}
 		}
 	},
-
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@import "src/variables.scss";'
-			}
-		})
-	]
+	
 };
+
+export default config;
